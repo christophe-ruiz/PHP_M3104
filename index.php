ee432d4dacc4f124c79e4051100a73940f3457ef
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-13 09:37:24 +0200

    2. Découverte PHP (étape 1)

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    $var1 = 6;
+    $var2 = 1.3;
+    $var3 = 'Variable 3';
+
     echo '<strong> Voici mon premier programme PHP </strong><br>' . "\n";
     echo 'C\'est pas mal non ?' . "\n";
 ?>
