b34824817286be8254c1c07767d5986762a93383
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-13 09:52:24 +0200

    3. Mutualisation du code (étape 1)

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    // 2. Decouverte du PHP
+    /*
     $var1 = 6;
     $var2 = 1.3;
     $var3 = 'Variable 3';
@@ -9,4 +11,17 @@
 
     echo '<strong> Voici mon premier programme PHP </strong><br>' . "\n";
     echo 'C\'est pas mal non ?' . "\n";
+    */
+
+    // 3. Mutualisation du code
+    function start_page ($title)
+    {
+        echo '<!DOCTYPE html> <html lang=fr>
+              <head>
+                <title>' . PHP_EOL . $title . '</title>
+              </head>
+              <body>' . PHP_EOL;
+    };
+
+    start_page('titre');
 ?>
