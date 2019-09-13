f06e9a145cbb287ed92c6d16c9921b885ad4f397
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-13 09:56:27 +0200

    3. Mutualisation du code (étape 2)

--- index.php
+++ index.php
@@ -24,4 +24,12 @@
     };
 
     start_page('titre');
+
+    function end_page (){
+        echo '</body>
+              </html>';
+    }
+
+    echo '<hr/><br><strong>Test</strong><br><hr>';
+    end_page();
 ?>
