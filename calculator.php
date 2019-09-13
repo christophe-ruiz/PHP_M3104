be61e44518ea639f57785bcab09ee8faee992642
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-13 10:52:01 +0200

    6. Factorisation du code

--- calculator.php
+++ calculator.php
@@ -1,15 +1,22 @@
 <?php
     require 'index.php';
+    $operateurs = '*+-/';
     start_page('Calculatrice');
     echo'
     <form action ="calcul.php" method="post">
         <input type="text" name ="op1">
-        <input type="text" name ="op2"> <br>
-        <input checked="checked" type="radio" name="op" value="*"/>*<br/>
-        <input type="radio" name="op" value="+"/>+<br/>
-        <input type="radio" name="op" value="-"/>-<br/>
-        <input type="radio" name="op" value="/"/>/<br/>
-        <input type="submit" value="Envoyer">
+        <input type="text" name ="op2"> <br>';
+
+    for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
+    {
+        echo '<input ';
+        if($cpt == 0)
+        {
+            echo 'checked="checked" ';
+        }
+        echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
+    }
+    echo '<input type="submit" value="Envoyer">
     </form>
         ';
     end_page();
